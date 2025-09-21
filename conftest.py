71e01062871cab73d200d265642300e0c6c0c74e
JiaminLo <jluo6565@conestogac.on.ca>
2025-09-21 18:21:06 -0400

    chore: fix ci compile error

--- conftest.py
+++ conftest.py
@@ -1,11 +1,20 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 # define a global fixture in conftest, other files can use it without importing
 # fixture: a decorator, used to start browser, connect db
 @pytest.fixture
 def browser():
-    driver=webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless")  # run in headless mode
+    options.add_argument(
+        "--no-sandbox"
+    )  # # needed for GitHub Actions, avoid permission error
+    options.add_argument("--disable-dev-shm-usage")  # overcome limited /dev/shm
+
+    driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(5)
     yield driver
     driver.quit()
